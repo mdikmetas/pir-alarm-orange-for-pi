9adedf0bb6555ce3df4667e8d808979a8bcbd761
mdikmetas <muratdikmetas@gmail.com>
2019-10-08 14:17:37 +0300

    Create pir_detectorforblynk mail.py

---
+++ pir_detectorforblynk mail.py
@@ -0,0 +1,33 @@
+from time import sleep
+from pyA20.gpio import gpio
+from pyA20.gpio import port
+
+import time
+import datetime
+import os
+import sys
+import blynklib
+
+blynk = blynklib.Blynk('scj8uYdDbHItzwxDiQOYkeBvX99HoZHO')
+
+gpio.init()
+hareket = port.PA20
+
+gpio.setcfg(hareket, gpio.INPUT)
+
+while True:
+    blynk.run()
+    if gpio.input(hareket):
+        print "hareketvar" " " + str(datetime.datetime.now())
+        time.sleep(1)
+#        blynk.run()
+        blynk.email("muratdikmetas@gmail.com", "CAFE ALARM", "CAFEDE HAREKET VAR");
+
+
+
+
+
+
+
+
+
